dc3010f8420ce291b92b2b654890e8441878b771
sjlo <sjlo@DESKTOP-H4PBPAP.bbrouter>
2025-05-10 13:32:02 -0400

    completed first part from TODO

---
+++ files_for_eval/arit.c
@@ -0,0 +1,11 @@
+#include "arit.h"
+
+int suma (int a, int b)
+{
+   return a+b;
+}
+
+int mult (int a, int b)
+{
+   return a*b;
+}
---
+++ files_for_eval/arit.h
@@ -0,0 +1,7 @@
+#ifndef _LIB_ARIT
+#define _LIB_ARIT
+
+int suma (int a, int b);
+int mult (int a, int b);
+
+#endif
